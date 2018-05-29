b5b8f59bbda73a78e58f0b04d707bcdbcce90a23
Timothy Pratley <timothypratley@gmail.com>
2018-05-28 20:23:23 -0700

    organizes build, builds slides and pdf

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
 set -e
+FLAGS=--destination-dir=site
 
-asciidoctor --destination-dir=site index.adoc manual/*.adoc slides/*.adoc
+bundle exec asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc
+bundle exec asciidoctor-pdf ${FLAGS} docs/manual.adoc
+bundle exec asciidoctor-revealjs ${FLAGS} docs/slides.adoc docs/advanced-topics.adoc docs/advanced-topics.adoc
