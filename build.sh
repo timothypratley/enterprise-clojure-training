8cde0420f4e12275fb8b41c77cf72109ce634a81
Timothy Pratley <timothypratley@gmail.com>
2018-06-04 17:18:23 -0700

    build with travisci

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 FLAGS=--destination-dir=site
 
-bundle exec asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc
-bundle exec asciidoctor-pdf ${FLAGS} docs/manual.adoc
-bundle exec asciidoctor-revealjs ${FLAGS} docs/slides.adoc docs/advanced-topics.adoc docs/advanced-topics.adoc
+asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc
+asciidoctor-pdf ${FLAGS} docs/manual.adoc
+asciidoctor-revealjs ${FLAGS} docs/slides.adoc docs/advanced-topics.adoc docs/advanced-topics.adoc
