c4917e61027cae53778ab270bb2d364d61103520
Timothy Pratley <timothypratley@gmail.com>
2018-09-11 21:31:15 -0700

    starting on transcribing the setup video instruction

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 FLAGS=--destination-dir=site
 
-asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc docs/getting-help.adoc
+asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc docs/getting-help.adoc docs/setup.adoc
 asciidoctor-pdf ${FLAGS} docs/manual.adoc
 asciidoctor-revealjs ${FLAGS} docs/slides.adoc docs/advanced-topics.adoc docs/advanced-topics.adoc
