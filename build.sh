03f1ba6151f2c026e106e110964db4ea412e133e
Timothy Pratley <timothypratley@gmail.com>
2019-02-04 17:37:36 -0800

    issue-14: Integration fixes for Klipse+RevealJS

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ FLAGS=--destination-dir=site
 
 asciidoctor ${FLAGS} docs/index.adoc docs/manual.adoc docs/getting-help.adoc docs/setup.adoc
 asciidoctor-pdf ${FLAGS} docs/manual.adoc
-asciidoctor-revealjs ${FLAGS} docs/slides.adoc docs/advanced-topics.adoc docs/advanced-topics.adoc docs/talk.adoc
+asciidoctor-revealjs ${FLAGS} docs/advanced-topics.adoc docs/advanced-topics.adoc docs/talk.adoc
+asciidoctor-revealjs ${FLAGS} docs/slides/*.adoc
